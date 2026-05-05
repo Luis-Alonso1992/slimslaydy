c19d120cf134aee81c270d63e1fdd2069db2d7a7
luis.alonso@alonsops.com <133282260+luis10333@users.noreply.github.com>
2026-05-05 12:18:43 -0400

    API call updated

--- RaidIO_API.py
+++ RaidIO_API.py
@@ -5,30 +5,36 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("api_key")
 
-url = "https://raider.io/api/v1/characters/profile"
+def get_character_information():
+    url = "https://raider.io/api/v1/characters/profile"
 
-params = {
-    "access_key": api_key,
-    "region": "us",
-    "realm": "Thunderlord",
-    "name": "SlimSlaydy",
-    "fields": "gear,mythic_plus_scores_by_season:current"
-}
+    params = {
+        "access_key": api_key,
+        "region": "us",
+        "realm": "Thunderlord",
+        "name": "SlimSlaydy",
+        "fields": "gear,mythic_plus_scores_by_season:current"
+    }
 
-response = requests.get(url, params=params)
-print("Status code:", response.status_code)
+    response = requests.get(url, params=params)
+    print("Status code:", response.status_code)
 
-if response.status_code == 200:
-    data = response.json()
-    season = data.get("mythic_plus_scores_by_season", [])
+    if response.status_code == 200:
+        data = response.json()
+        season = data.get("mythic_plus_scores_by_season", [])
+        current_season = season[0]
+        scores = current_season.get("scores", {})
+        return {
+            "name": data.get("name"),
+            "realm": data.get("realm"),
+            "race": data.get("race"),
+            "class": data.get("class"),
+            "mythic_score": scores.get("all")
+        }
+    else:
+        print("Request Failed")
+        print(response.text[:500])
 
-    print ("Name:", data.get("name"))
-    print ("Realm:", data.get("realm"))
-    print ("Race:", data.get("race"))
-    print ("Class:", data.get("class"))
-    current_season = season[0]
-    scores = current_season.get("scores", {})
-    print ("Mythic+ Score:", scores.get("all"))
-else:
-    print("Request Failed")
-    print(response.txt[:500])
+
+get_character_information()
+
