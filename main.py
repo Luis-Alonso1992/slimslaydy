9cb53e47dc4ee4f00b561938a1037ee89ab80f97
luis.alonso@alonsops.com <133282260+luis10333@users.noreply.github.com>
2026-05-05 13:07:42 -0400

    Added a web dashboard

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from raiderio import get_character_information
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def dashboard(request: Request):
+    character = get_character_information()
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html", 
+        context={"character": character,}      
+    )
